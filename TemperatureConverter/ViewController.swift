8f453dcfa8e0e22a26e4cdb4e24e86fdb133d638
sanmaowang <wang@sanmao.me>
2014-10-26 21:08:52 +0800

    Initial Commit

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Sanmao on 14-10-26.
+//  Copyright (c) 2014年 Sanmao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
