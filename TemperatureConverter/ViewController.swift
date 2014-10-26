99e553ae19ed7df60d59537e728e2dd0fe0cb303
sanmaowang <wang@sanmao.me>
2014-10-26 22:20:54 +0800

    init

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    
+    @IBOutlet weak var temperatureSlider: UISlider!
+    
+    @IBAction func temperatureSliderChanged(sender: UISlider){
+        self.updateTemperatureDisplays()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +29,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        let screen = UIScreen.mainScreen()
+//        println("screen fixed bounds: \(screen.fixedCoordinateSpace.bounds)")
+//        // Since ios8, this is the size of the screen in its current orientation
+//        println("screen bounds: \(screen.bounds)")
+//        
+//        println("top layout guide: \(topLayoutGuide.length)")
+//        
+//        titleLabel.frame = CGRectMake(16, 16, screen.bounds.width-32, 21)
+//    }
+    func updateTemperatureDisplays(){
+        var c = self.temperatureSlider.value
+        var f = (c * 9/5) + 32
+        var k = c + 273.15
+//        self.celsiusLabel.text = "\(c)C"
+        self.celsiusLabel.text = String(format: "%.1fC", arguments: [c])
+        self.fahrenheitLabel.text = String(format: "%.1fF", arguments: [f])
+        self.kelvinLabel.text = String(format:"%.2fK",arguments:[k])
     }
 
 
